0c46740468442e79316c89948fde39a6c63b9de5
mizokei <>
2019-09-15 05:47:50 +0900

    新規作成

---
+++ src/javasampleapplication/Java01/JavaSampleCode01.java
@@ -0,0 +1,18 @@
+/*
+ * 「Javaサンプルコード集」用ソースコード by mizokei
+ */
+package javasampleapplication.Java01;
+
+/**
+ * 1-1 Hello Java World !! 
+ */
+public class JavaSampleCode01 {
+
+    public static void main(String[] args) {
+        
+        //「Hello Java World!!」を表示する
+        System.out.println("Hello Java World!!");
+        
+    }
+    
+}
