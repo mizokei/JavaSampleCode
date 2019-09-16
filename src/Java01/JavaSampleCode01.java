fb17eafca1546e802a26f341cc7ce61acb922375
mizokei <>
2019-09-16 16:29:58 +0900

    フォルダ名を修正

---
+++ src/Java01/JavaSampleCode01.java
@@ -0,0 +1,18 @@
+/*
+ * 「Javaサンプルコード集」用ソースコード by mizokei
+ */
+package Java01;
+
+/**
+ * 0-1 文字「Hello Java World !!」を表示する
+ */
+public class JavaSampleCode01 {
+
+    public static void main(String[] args) {
+        
+        //「Hello Java World!!」を表示する
+        System.out.println("Hello Java World!!");
+        
+    }
+    
+}
---
+++ src/Java02/JavaSampleCode02.java
@@ -0,0 +1,30 @@
+/*
+ * 「Javaサンプルコード集」用ソースコード by mizokei
+ */
+package Java02;
+
+/**
+ * 0-2 四則演算を行う
+ */
+public class JavaSampleCode02 {
+
+    public static void main(String[] args) {
+        
+        //足し算を表示する
+        System.out.println("19+14は"+(19+14)+"です。");
+        
+        //引き算を表示する
+        System.out.println("25-13は"+(25-13)+"です。");
+        
+        //掛け算を表示する
+        System.out.println("54*2は"+(54*2)+"です。");
+        
+        //割り算を表示する
+        System.out.println("24/3は"+(24/3)+"です。");
+
+        //割った余りを表示する
+        System.out.println("18%4は"+(18%4)+"です。");
+        
+    }
+    
+}
