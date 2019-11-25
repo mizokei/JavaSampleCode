ee4a51e83a411709de638de94b18291b03fbe25b
mizokei <>
2019-11-25 23:22:07 +0900

    「消費税の計算」を追加

---
+++ src/Java05/JavaSampleCode05.java
@@ -0,0 +1,18 @@
+/*
+ * 「Javaサンプルコード集」用ソースコード by mizokei
+ */
+package Java05;
+
+/**
+ *  0-5 消費税を計算する
+ */
+public class JavaSampleCode05 {
+ 
+    public static void main(String[] args) {
+
+        //計算した消費税を表示する
+        System.out.println("購入したチョコレートの消費税は"+((450*3)*0.1)+"円です。");
+
+    }
+    
+}
