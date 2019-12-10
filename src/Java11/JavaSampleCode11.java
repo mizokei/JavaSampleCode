834fa83e84f3a8eb940b2969351193b961dd7245
mizokei <>
2019-12-10 19:54:53 +0900

    「変数を使って文字を表示」を追加

---
+++ src/Java11/JavaSampleCode11.java
@@ -0,0 +1,21 @@
+/*
+ * 「Javaサンプルコード集」用ソースコード by mizokei
+ */
+package Java11;
+
+/**
+ *  1-1 変数を使って文字「Hello Java World !!」を表示する
+ */
+public class JavaSampleCode11 {
+
+    public static void main(String[] args) {
+        
+        //「Hello Java World!!」を変数wordに入れる
+        String word = "Hello Java World!!";
+        
+        //変数に入れた「Hello Java World!!」を表示する
+        System.out.println(word);
+        
+    }
+    
+}
